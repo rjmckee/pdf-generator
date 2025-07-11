ce56f0fb5bf50505ab670c9ba003ec3376dbc683
rjmckee <102568233+rjmckee@users.noreply.github.com>
2025-07-11 10:24:57 -0400

    Update PDF_generator.py

--- PDF_generator.py
+++ PDF_generator.py
@@ -14,13 +14,3 @@ if index != -1:
    c.save()
 else:
    print("Oops, did you forget to put .pdf file extension? Please try again ")
-
-
-
-
-# Ask user for text to put in file
-#text = input("Type what you want in the file: ")
-
-#c = canvas.Canvas(pdf_file)
-#c.drawString(100, 750, text)
-#c.save()
