e4095c7a5bfb202de3bc52f9ec9d2184e1abbfd4
rjmckee <102568233+rjmckee@users.noreply.github.com>
2025-07-11 10:22:51 -0400

    Add files via upload

---
+++ PDF_generator.py
@@ -0,0 +1,26 @@
+from reportlab.pdfgen import canvas
+
+# Ask user for file name
+pdf_file = input("Type file name + .pdf: ")
+
+# Check to ensure '.pdf' was included in file type
+index = pdf_file.find(".pdf")
+if index != -1:
+   print("Correct file name!")
+   # Ask user for text to put in file
+   text = input("Type what you want in the file: ")
+   c = canvas.Canvas(pdf_file)
+   c.drawString(100, 750, text)
+   c.save()
+else:
+   print("Oops, did you forget to put .pdf file extension? Please try again ")
+
+
+
+
+# Ask user for text to put in file
+#text = input("Type what you want in the file: ")
+
+#c = canvas.Canvas(pdf_file)
+#c.drawString(100, 750, text)
+#c.save()
